d7732f1aec9e665e3ddd36f1f693883f99c86f39
　yangrui <yangrui@yangruideMini.lan>
2017-07-20 13:19:23 +0800

    xx

---
+++ Example/wavyView/YRWavyView.h
@@ -0,0 +1,13 @@
+//
+//  YRWavyView.h
+//  wavyView
+//
+//  Created by 　yangrui on 2017/7/20.
+//  Copyright © 2017年 yangrui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YRWavyView : UIView
+
+@end
---
+++ Example/wavyView/YRWavyView.m
@@ -0,0 +1,21 @@
+//
+//  YRWavyView.m
+//  wavyView
+//
+//  Created by 　yangrui on 2017/7/20.
+//  Copyright © 2017年 yangrui. All rights reserved.
+//
+
+#import "YRWavyView.h"
+
+@implementation YRWavyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
